f05ba2218c7650a3abfd36d2dd50c923968c4143
renzee-r <rj4219@gmail.com>
2019-12-06 12:46:41 -1000

    Added breakpoints for producthero

--- src/modules/ProductHero.js
+++ src/modules/ProductHero.js
@@ -14,6 +14,7 @@ import 'debug.addIndicators'
 
 let theme = createMuiTheme();
 theme = responsiveFontSizes(theme);
+const { breakpoints } = theme;
 
 const styles = theme => ({
     root: {
@@ -31,7 +32,12 @@ const styles = theme => ({
         alignItems: 'center',
     },
     heroContent: {
-        paddingTop: '33vh',
+        [theme.breakpoints.down('sm')]: {
+            paddingTop: '20vh',
+        },
+        [theme.breakpoints.up('md')]: {
+            paddingTop: '35vh',
+        },
         width: '50vw',
     },
     scrollStart: {
